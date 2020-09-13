95070e6c7357310da55f36b8702240a6af18381c
apex-coder <micnic123@gmail.com>
2020-09-13 19:28:15 -0400

    Login Page CSS Stylings

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,10 @@
 import React, { useCallback, useContext} from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
+import { withRouter, Redirect, Link } from 'react-router-dom';
 import * as firebase from 'firebase/app';
 import app from '../../Configuration/base';
 import { AuthContext } from '../../Configuration/Auth';
 
-import './Login.css'
+import './Login.css';
 
 const Login = ({ history }) => {
     const handleLogin = useCallback(
@@ -48,18 +48,29 @@ const Login = ({ history }) => {
     }
 
     return (
-        <div>
-            <h1>Log In</h1>
-            <form onSubmit={handleLogin} className="form-style">
-                <label>Email</label><br />
-                <input name="email" type="email" placeholder="Email" />
-                <br />
-                <label>Password</label><br />
-                <input name="password" type="password" placeholder="Password" /><br />
-                <button type="submit">Log In</button>
-                
-                <button type="button" onClick={handleGoogleLogin}>Log In with Google</button>
-            </form>
+        <div className="hero-image">
+            <div className="form-container">
+                <h1>Log In</h1>
+                <p>Welcome back! Let's track those finances</p>
+                <form onSubmit={handleLogin} className="form-style">
+                    <label>Email</label><br />
+                    <input name="email" type="email" placeholder="Email" />
+                    <br />
+                    <label>Password</label><br />
+                    <input name="password" type="password" placeholder="Password" /><br />
+                    <button type="submit" id="login-btn">Log In</button>
+                    
+                    <div className="btn-container">
+                        <Link to="/signup" id="notSignedUp" >Don't have an account</Link>
+                        <div className="google-btn">
+                            <div className="google-icon-wrapper">
+                                <div className="google-icon"></div>
+                            </div>
+                            <p className="btn-text" onClick={handleGoogleLogin}><b>Log in with google</b></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
